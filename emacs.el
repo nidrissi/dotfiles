24fc61c51975cb7e18eeb61a7332ab904a146643
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2017-03-10 14:17:31 +0100

    Clean up .emacs

--- emacs.el
+++ emacs.el
@@ -11,7 +11,6 @@
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Init
 (fset 'yes-or-no-p 'y-or-n-p)
 (require 'package)
@@ -53,18 +52,15 @@
 
 ;; dired
 (use-package dired
-  :defer t
-  :config
-  (define-key dired-mode-map (kbd "K") 'dired-k)
-  (define-key dired-mode-map (kbd "g") 'dired-k)
+  :commands dired
+  :bind
+  (:map dired-mode-map
+        ("K" . dired-k)
+        ("g" . dired-k))
+  :init
   (add-hook 'dired-initial-position-hook 'dired-k)
   (add-hook 'dired-after-readin-hook #'dired-k-no-revert))
-(use-package dired-k
-  :commands (dired-k direx-k))
-(use-package direx
-  :bind ("C-$" . direx-project:jump-to-project-root-other-window)
-  :config
-  (add-hook 'direx:direx-mode-hook 'direx-k))
+(use-package dired-k :commands dired-k)
 
 (cua-mode t)
 
@@ -114,11 +110,14 @@
   (eldoc-mode +1)
   (tide-hl-identifier-mode +1))
 (use-package tide
-  :mode "\\.ts\\'"
+  :commands tide-setup
   :config
-  (add-hook 'before-save-hook 'tide-format-before-save)
-  (add-hook 'typescript-mode-hook #'setup-tide-mode)
   (setq tide-format-options '(:insertSpaceAfterFunctionKeywordForAnonymousFunctions t :placeOpenBraceOnNewLineForFunctions nil)))
+(use-package typsecript-mode
+  :mode "\\.ts'"
+  :init
+  (add-hook 'before-save-hook 'tide-format-before-save)
+  (add-hook 'typescript-mode-hook 'setup-tide-mode))
 
 ;;; Misc prog
 (use-package cperl-mode
@@ -130,9 +129,9 @@
          "\\.phtml\\'" "\\.tpl\\.php\\'" "\\.[agj]sp\\'" "\\.as[cp]x\\'"
          "\\.erb\\'" "\\.mustache\\'" "\\.djhtml\\'" "\\.html?\\'"))
 (use-package sass-mode :mode "\\.scss?\\'")
-(use-package jade-mode :mode ("\\.jade\\'" . jade-mode))
+(use-package jade-mode :mode "\\.jade\\'")
 (use-package rainbow-delimiters
-  :config
+  :init
   (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
 
 ;; Magit
@@ -151,7 +150,7 @@
   (add-to-list 'reftex-bibliography-commands "addbibresource"))
 
 (use-package latex
-  :mode "\\.tex\\'"
+  :mode ("\\.tex'" . latex-mode)
   :init
   (setq ispell-tex-skip-alists
         (list
@@ -162,76 +161,79 @@
             ("\\\\import" ispell-tex-arg-end 2)
             ("\\\\textcite" ispell-tex-arg-end)))
          (cadr ispell-tex-skip-alists)))
-  :config
+
+  ;; hooks
   (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
   (add-hook 'LaTeX-mode-hook 'turn-on-flyspell)
   (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
   (add-hook 'LaTeX-mode-hook 'TeX-fold-mode)
   (add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
   (add-hook 'LaTeX-mode-hook 'prettify-symbols-mode)
+  (add-hook 'LaTeX-mode-hook
+            (lambda ()
+              (setq TeX-command-default "LaTeXmk"
+                    ;; I don't know why AUCTeX devs think they know better...
+                    company-minimum-prefix-length 3)))
+
+  :config
   (add-to-list 'LaTeX-font-list '(11 "" "" "\\mathfrak{" "}"))
 
-   ;; Fold
-   (add-to-list 'LaTeX-fold-macro-spec-list '("[r]" ("cref" "Cref")))
-   (add-to-list 'LaTeX-fold-macro-spec-list '("[c]" ("textcite")))
-   (add-to-list 'LaTeX-fold-macro-spec-list '("[f]" ("tablefootnote")))
-   (add-to-list 'LaTeX-fold-macro-spec-list '("[n]" ("nomenclature")))
-   (add-to-list 'LaTeX-fold-math-spec-list '("[" ("lbrack")))
-   (add-to-list 'LaTeX-fold-math-spec-list '("]" ("rbrack")))
-   (add-to-list 'LaTeX-fold-math-spec-list '("\u00ab" ("og")))
-   (add-to-list 'LaTeX-fold-math-spec-list '("\u00bb" ("fg")))
-   (add-to-list 'tex--prettify-symbols-alist '("\\varphi" . 966))
-   (add-to-list 'tex--prettify-symbols-alist '("\\coloneqq" . 8788))
+  ;; Fold
+  (add-to-list 'LaTeX-fold-macro-spec-list '("[r]" ("cref" "Cref")))
+  (add-to-list 'LaTeX-fold-macro-spec-list '("[c]" ("textcite")))
+  (add-to-list 'LaTeX-fold-macro-spec-list '("[f]" ("tablefootnote")))
+  (add-to-list 'LaTeX-fold-macro-spec-list '("[n]" ("nomenclature")))
+  (add-to-list 'LaTeX-fold-math-spec-list '("[" ("lbrack")))
+  (add-to-list 'LaTeX-fold-math-spec-list '("]" ("rbrack")))
+  (add-to-list 'LaTeX-fold-math-spec-list '("\u00ab" ("og")))
+  (add-to-list 'LaTeX-fold-math-spec-list '("\u00bb" ("fg")))
+  (add-to-list 'tex--prettify-symbols-alist '("\\varphi" . 966))
+  (add-to-list 'tex--prettify-symbols-alist '("\\coloneqq" . 8788))
 
-   ;; reftex
-   (TeX-add-style-hook
-    "cleveref"
-    (lambda ()
-      (if (boundp 'reftex-ref-style-alist)
-          (add-to-list
-           'reftex-ref-style-alist
-           '("Cleveref" "cleveref"
-             (("\\cref" ?c) ("\\Cref" ?C) ("\\cpageref" ?d) ("\\Cpageref" ?D)))))
-      (reftex-ref-style-activate "Cleveref")
-      (TeX-add-symbols
-       '("cref" TeX-arg-ref)
-       '("Cref" TeX-arg-ref)
-       '("cpageref" TeX-arg-ref)
-       '("Cpageref" TeX-arg-ref))))
+  ;; reftex
+  (TeX-add-style-hook
+   "cleveref"
+   (lambda ()
+     (if (boundp 'reftex-ref-style-alist)
+         (add-to-list
+          'reftex-ref-style-alist
+          '("Cleveref" "cleveref"
+            (("\\cref" ?c) ("\\Cref" ?C) ("\\cpageref" ?d) ("\\Cpageref" ?D)))))
+     (reftex-ref-style-activate "Cleveref")
+     (TeX-add-symbols
+      '("cref" TeX-arg-ref)
+      '("Cref" TeX-arg-ref)
+      '("cpageref" TeX-arg-ref)
+      '("Cpageref" TeX-arg-ref))))
 
-   ;; LaTeXmk
-   (add-to-list
-    'TeX-command-list
-    '("LaTeXmk" "latexmk %(-PDF) %(-xelatex) %s" TeX-run-TeX nil t
-      :help "Run Latexmk on file"))
-   (add-to-list
-    'TeX-expand-list
-    '("%(-PDF)"
-      (lambda ()
-        (if (or TeX-PDF-mode TeX-DVI-via-PDFTeX)
-            "-pdf" ""))))
-   (add-to-list
-    'TeX-expand-list
-    '("%(-xelatex)"
-      (lambda ()
-        (if (eq TeX-engine 'xetex)
-            "-e \"$pdflatex =~ s/pdflatex/xelatex/\"" ""))))
-   (add-hook 'LaTeX-mode-hook
-             (lambda ()
-               (setq TeX-command-default "LaTeXmk"
-                     ;; I don't know why AUCTeX devs think they know better...
-                     company-minimum-prefix-length 3)))
+  ;; LaTeXmk
+  (add-to-list
+   'TeX-command-list
+   '("LaTeXmk" "latexmk %(-PDF) %(-xelatex) %s" TeX-run-TeX nil t
+     :help "Run Latexmk on file"))
+  (add-to-list
+   'TeX-expand-list
+   '("%(-PDF)"
+     (lambda ()
+       (if (or TeX-PDF-mode TeX-DVI-via-PDFTeX)
+           "-pdf" ""))))
+  (add-to-list
+   'TeX-expand-list
+   '("%(-xelatex)"
+     (lambda ()
+       (if (eq TeX-engine 'xetex)
+           "-e \"$pdflatex =~ s/pdflatex/xelatex/\"" ""))))
 
-   ;; Windows
-   (if (eq system-type 'windows-nt)
-       (progn
-         (setq preview-gs-command "gswin64c.exe")
-         (add-to-list 'TeX-view-program-list
-                      '("Sumatra PDF"
-                        ("\"C:/Program Files/SumatraPDF/SumatraPDF.exe\" -reuse-instance"
-                         (mode-io-correlate " -forward-search %b %n") " %o")))
-         (assq-delete-all 'output-pdf TeX-view-program-selection)
-         (add-to-list 'TeX-view-program-selection '(output-pdf "Sumatra PDF")))))
+  ;; Windows
+  (if (eq system-type 'windows-nt)
+      (progn
+        (setq preview-gs-command "gswin64c.exe")
+        (add-to-list 'TeX-view-program-list
+                     '("Sumatra PDF"
+                       ("\"C:/Program Files/SumatraPDF/SumatraPDF.exe\" -reuse-instance"
+                        (mode-io-correlate " -forward-search %b %n") " %o")))
+        (assq-delete-all 'output-pdf TeX-view-program-selection)
+        (add-to-list 'TeX-view-program-selection '(output-pdf "Sumatra PDF")))))
 
 ;;; Fonts (used for folding)
 (dolist (range '((#x2200 . #x23ff) (#x27c0 . #x27ef) (#x2980 . #x2bff) (#x1d400 . #x1d7ff)))
