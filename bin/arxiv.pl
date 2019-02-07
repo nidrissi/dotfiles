5d8d71722d58a027d25f065b43e6839f6e79206f
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2019-02-07 10:05:06 +0100

    arxiv: pubstate

--- bin/arxiv.pl
+++ bin/arxiv.pl
@@ -34,7 +34,7 @@ for my $entry (@entry) {
     } $entry->children('author');
     my $author_list = join ' and ', @author;
 
-    printf '@misc{tmpkey, author = {%s}, title = {%s}, year = {%s}, eprint = {%s}, eprinttype = {arXiv}, note = {Preprint}}',
+    printf '@misc{tmpkey, author = {%s}, title = {%s}, year = {%s}, eprint = {%s}, eprinttype = {arXiv}, pubstate = {prepublished}}',
       $author_list, $title, $date, $id;
     print "\n";
 }
