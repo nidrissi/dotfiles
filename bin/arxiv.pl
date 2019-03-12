54d4d247fef5e116aa9783f0072de521f31c33a3
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2019-03-12 14:48:25 +0100

    arxiv: key

--- bin/arxiv.pl
+++ bin/arxiv.pl
@@ -8,6 +8,9 @@ use LWP::Simple;
 use XML::Twig;
 #use Date::Parse;
 
+use File::Spec::Functions;
+my $pdf_dir = catfile $ENV{HOME}, 'ownCloud', 'papers';
+
 my $twig=XML::Twig->new();
 
 die 'Usage: arxiv <id>' unless $#ARGV == 0;
@@ -16,24 +19,28 @@ $twig->parse(get("http://export.arxiv.org/api/query?search_query=id:$ARGV[0]"));
 my $root = $twig->root;
 my @entry = $root->children('entry');
 for my $entry (@entry) {
-    my $title = $entry->first_child('title')->text;
+    my $title = $entry->first_child_text('title');
 
     # arXiv id
-    my $id = $entry->first_child('id')->text;
+    my $id = $entry->first_child_text('id');
     $id =~ s{http://arxiv.org/abs/(.+)v(?:\d)+}{$1};
 
     # Year
-    my $year = $entry->first_child('published')->text;
+    my $year = $entry->first_child_text('published');
     $year =~ s/^(....).*/$1/;
 
     # Author list
-    my @author = map {
-        # Najib Idrissi -> Idrissi, Najib
-        # Assumes only the last word is the surname
-        $_->text =~ s/^(.+) ([^ ]+)$/$2, $1/r;
-    } $entry->children('author');
-    my $author_list = join ' and ', @author;
+    my @author = $entry->children_text('author');
+    my @processed_authors;
+    my $key;
+    for my $author (@author) {
+        $author =~ /^(.+) ([^ ]+)$/;
+        $key .= $2;
+        push @processed_authors, "$2, $1";
+    }
+    my $author_list = join ' and ', @processed_authors;
+    $key .= $year;
 
-    printf '@misc{tmpkey, author = {%s}, title = {%s}, year = {%s}, eprint = {%s}, eprinttype = {arXiv}, pubstate = {prepublished}}' . "\n",
-      $author_list, $title, $year, $id;
+    printf '@misc{%s, author = {%s}, title = {%s}, year = {%s}, eprint = {%s}, eprinttype = {arXiv}, pubstate = {prepublished}}' . "\n",
+      $key, $author_list, $title, $year, $id;
 }
