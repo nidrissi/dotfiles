edc759d0a1a0b549d53fc84db39a3fd4cb3b0111
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2019-12-20 15:58:10 +0100

    fix org-mode

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -208,7 +208,7 @@
  '(org-use-speed-commands t)
  '(package-selected-packages
    (quote
-    (cdlatex org rust-mode elpy lua-mode pov-mode glsl-mode julia-repl tuareg julia-mode ebib graphviz-dot-mode ivy smart-mode-line json-mode go-mode ssh-agency tup-mode openwith-mode jade-mode sass-mode web-mode tide multiple-cursors diff-hl move-text gitconfig-mode gitignore-mode ivy-hydra counsel-projectile counsel anzu volatile-highlights-mode volatile-highlights smartparens company-emoji auctex-latexmk projectile-mode ace-jump-mode esup use-package zenburn-theme yaml-mode visual-fill-column unicode-fonts undo-tree smart-mode-line-powerline-theme rainbow-delimiters openwith markdown-mode magit diminish cperl-mode company color-theme browse-kill-ring auctex ace-window)))
+    (org cdlatex rust-mode elpy lua-mode pov-mode glsl-mode julia-repl tuareg julia-mode ebib graphviz-dot-mode ivy smart-mode-line json-mode go-mode ssh-agency tup-mode openwith-mode jade-mode sass-mode web-mode tide multiple-cursors diff-hl move-text gitconfig-mode gitignore-mode ivy-hydra counsel-projectile counsel anzu volatile-highlights-mode volatile-highlights smartparens company-emoji auctex-latexmk projectile-mode ace-jump-mode esup use-package zenburn-theme yaml-mode visual-fill-column unicode-fonts undo-tree smart-mode-line-powerline-theme rainbow-delimiters openwith markdown-mode magit diminish cperl-mode company color-theme browse-kill-ring auctex ace-window)))
  '(prettify-symbols-unprettify-at-point t)
  '(preview-gs-command "gswin64c.exe")
  '(preview-gs-options
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -3,7 +3,13 @@
 ;; I have a lot of RAM.
 (setq gc-cons-threshold 100000000)
 
+;; Very annoying
+;; https://stackoverflow.com/a/17422623
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
 (require 'org)
+
 (setq vc-follow-symlinks t)
 (org-babel-load-file
  (expand-file-name "emacs.org"
