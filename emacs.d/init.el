11691358db9e0bdd158d754d64ecc09e6d8426bc
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-05-02 10:19:50 +0200

    my/tangle?

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -10,6 +10,9 @@
 (package-initialize)
 (require 'org)
 
+;; to tangle or not tangle, that is the question
+(defun my/tangle? () (if window-system "yes" "no"))
+
 (setq vc-follow-symlinks t)
 (if (eq system-type 'windows-nt)
     (delete-file (expand-file-name "emacs.el" user-emacs-directory)))
