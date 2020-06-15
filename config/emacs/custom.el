261df78f40976e6bf274903863af68190d0ea00b
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-06-15 18:19:22 +0200

    unused conf

--- config/emacs/custom.el
+++ config/emacs/custom.el
@@ -5,7 +5,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit aw-mode-line-face :height 3.0))))
  '(highlight ((t (:background "#0a4b5b"))))
  '(org-archived ((t (:foreground "dim gray" :weight normal))))
  '(org-todo ((t (:foreground "firebrick" :weight bold)))))
@@ -59,7 +58,6 @@
      (output-dvi "xdvi")
      (output-html "xdg-open")))
  '(auctex-latexmk-inherit-TeX-PDF-mode t)
- '(aw-keys '(38 233 34 39 40 45 232 95 231 224))
  '(backup-by-copying t)
  '(bibtex-autokey-name-case-convert-function 'identity)
  '(bibtex-autokey-names 'infty)
@@ -70,7 +68,6 @@
  '(bibtex-autokey-year-title-separator "")
  '(blink-cursor-mode nil)
  '(calendar-week-start-day 1)
- '(cdlatex-math-symbol-prefix 178)
  '(column-number-mode t)
  '(company-idle-delay 0.0)
  '(company-minimum-prefix-length 1)
@@ -80,9 +77,6 @@
  '(confirm-kill-emacs 'yes-or-no-p)
  '(counsel-find-file-ignore-regexp
    "\\(?:-blx\\.bib\\|\\.\\(?:386\\|aux\\|b\\(?:ak\\|bl\\|cf\\|in\\|lg\\|zr/\\)\\|c\\(?:lass\\|ps?\\)\\|d\\(?:64fsl\\|fsl\\|ll\\|rv\\|x\\(?:\\(?:\\(?:32\\|64\\)fs\\)?l\\)\\)\\|elc\\|f\\(?:asl?\\|db_latexmk\\|ls\\|mt\\|ns?\\|\\(?:x\\(?:\\(?:32\\|64\\)f\\)\\)?sl\\)\\|g\\(?:it/\\|[lm]o\\)\\|h\\(?:g/\\|i\\)\\|i\\(?:dx\\|lg\\|nd\\)\\|kys?\\|l\\(?:ib\\|nk\\|o[fgt]\\|x\\(?:\\(?:32\\|64\\)fsl\\)\\|[ano]\\)\\|m\\(?:ap\\|em\\|o\\)\\|nav\\|o\\(?:bj\\|ut\\)\\|p\\(?:64fsl\\|fsl\\|gs?\\|if\\|y[co]\\)\\|run\\.xml\\|s\\(?:nm\\|parcf\\|vn/\\|x\\(?:\\(?:32\\|64\\)fsl\\)\\|ynctex\\.gz\\)\\|t\\(?:do\\|fm\\|oc\\|ps?\\)\\|ufsl\\|v\\(?:rs?\\|xd\\)\\|wx\\(?:\\(?:32\\|64\\)fsl\\)\\|x86f\\|[ao]\\)\\|CVS/\\|_\\(?:\\(?:MTN\\|darcs\\)/\\)\\|~\\)\\'")
- '(cperl-close-paren-offset -4)
- '(cperl-indent-level 4)
- '(cperl-indent-parens-as-block t)
  '(create-lockfiles nil)
  '(custom-safe-themes
    '("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "2997ecd20f07b99259bddba648555335ffb7a7d908d8d3e6660ecbec415f6b95" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "a802c77b818597cc90e10d56e5b66945c57776f036482a033866f5f506257bca" default))
@@ -90,10 +84,7 @@
     '((font . "Cascadia Code-11")
       (vertical-scroll-bars . right)))
  '(delete-old-versions t)
- '(dired-async-mode t)
  '(dired-listing-switches "-alh")
- '(display-time-mode t)
- '(display-time-string-forms '(24-hours ":" minutes))
  '(ebib-allow-identical-fields t)
  '(ebib-bib-search-dirs '("/home/najib/math/texmf/bibtex/bib") t)
  '(ebib-bibtex-dialect 'biblatex)
@@ -194,40 +185,6 @@
  '(org-display-custom-times t)
  '(org-enforce-todo-checkbox-dependencies t)
  '(org-enforce-todo-dependencies t)
- '(org-format-latex-options
-   '(:foreground default :background default :scale 1.5 :html-foreground "Black" :html-background "Transparent" :html-scale 1.0 :matchers
-                 ("begin" "$1" "$" "$$" "\\(" "\\[")))
- '(org-latex-classes
-   '(("beamer" "\\documentclass[presentation]{beamer}"
-      ("\\section{%s}" . "\\section*{%s}")
-      ("\\subsection{%s}" . "\\subsection*{%s}")
-      ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))
-     ("article" "\\documentclass[11pt]{article}"
-      ("\\section{%s}" . "\\section*{%s}")
-      ("\\subsection{%s}" . "\\subsection*{%s}")
-      ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-      ("\\paragraph{%s}" . "\\paragraph*{%s}")
-      ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
-     ("report" "\\documentclass[11pt]{report}"
-      ("\\part{%s}" . "\\part*{%s}")
-      ("\\chapter{%s}" . "\\chapter*{%s}")
-      ("\\section{%s}" . "\\section*{%s}")
-      ("\\subsection{%s}" . "\\subsection*{%s}")
-      ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))
-     ("book" "\\documentclass[11pt]{book}"
-      ("\\part{%s}" . "\\part*{%s}")
-      ("\\chapter{%s}" . "\\chapter*{%s}")
-      ("\\section{%s}" . "\\section*{%s}")
-      ("\\subsection{%s}" . "\\subsection*{%s}")
-      ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))
-     ("scrartcl" "\\documentclass{scrartcl}"
-      ("\\section{%s}" . "\\section*{%s}")
-      ("\\subsection{%s}" . "\\subsection*{%s}")
-      ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-      ("\\paragraph{%s}" . "\\paragraph*{%s}")
-      ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))))
- '(org-latex-pdf-process
-   '("latexmk -pdf -pdflatex=%latex --synctex=1 -interaction=nonstopmode  -file-line-error -shell-escape %f"))
  '(org-log-done 'time)
  '(org-lowest-priority 68)
  '(org-modules '(ol-bibtex ol-docview ol-eww ol-info))
@@ -254,9 +211,6 @@
  '(package-selected-packages
    '(doom-themes composite json-mode reftex htmlize typescript-mode which-key lsp-ivy lsp-python-ms esup adaptive-wrap lsp-treemacs flycheck lsp-ui lsp-mode all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-dired diredfl uniquify dired-x smartparens-mode org elpy lua-mode glsl-mode julia-repl tuareg julia-mode ebib graphviz-dot-mode ivy smart-mode-line go-mode ssh-agency tup-mode openwith-mode sass-mode web-mode tide multiple-cursors diff-hl move-text gitconfig-mode gitignore-mode ivy-hydra counsel-projectile counsel anzu volatile-highlights-mode volatile-highlights smartparens company-emoji auctex-latexmk projectile-mode use-package yaml-mode visual-fill-column unicode-fonts undo-tree smart-mode-line-powerline-theme rainbow-delimiters openwith markdown-mode magit diminish cperl-mode company color-theme auctex ace-window))
  '(prettify-symbols-unprettify-at-point t)
- '(preview-gs-command "gswin64c.exe")
- '(preview-gs-options
-   '("-q" "-dNOPAUSE" "-DNOPLATFONTS" "-dPrinted" "-dTextAlphaBits=4" "-dGraphicsAlphaBits=4"))
  '(projectile-completion-system 'ivy)
  '(projectile-globally-ignored-file-suffixes
    '(".hi" ".o" "~" ".bin" ".bak" ".obj" ".map" ".pif" ".lnk" ".a" ".ln" ".blg" ".bbl" ".dll" ".drv" ".vxd" ".386" ".elc" ".lof" ".glo" ".idx" ".lot" ".svn/" ".hg/" ".git/" ".bzr/" "CVS/" "_darcs/" "_MTN/" ".fmt" ".tfm" ".class" ".fas" ".lib" ".mem" ".x86f" ".sparcf" ".dfsl" ".pfsl" ".d64fsl" ".p64fsl" ".lx64fsl" ".lx32fsl" ".dx64fsl" ".dx32fsl" ".fx64fsl" ".fx32fsl" ".sx64fsl" ".sx32fsl" ".wx64fsl" ".wx32fsl" ".fasl" ".ufsl" ".fsl" ".dxl" ".lo" ".la" ".gmo" ".mo" ".toc" ".aux" ".cp" ".fn" ".ky" ".pg" ".tp" ".vr" ".cps" ".fns" ".kys" ".pgs" ".tps" ".vrs" ".pyc" ".pyo" ".fls" ".fdb_latexmk" ".run.xml" ".synctex.gz" "-blx.bib" ".nav" ".out" ".snm" ".log" ".bcf" ".ilg" ".ind" ".tdo"))
@@ -266,7 +220,7 @@
    '("/home/najib/.config/emacs/elpa" "/\\(\\(\\(COMMIT\\|NOTES\\|PULLREQ\\|MERGEREQ\\|TAG\\)_EDIT\\|MERGE_\\|\\)MSG\\|\\(BRANCH\\|EDIT\\)_DESCRIPTION\\)\\'"))
  '(reftex-derive-label-parameters
    '(3 20 t 1 "-"
-       ("the" "on" "in" "off" "a" "for" "by" "of" "and" "is" "to" "et" "le" "la" "les" "un" "une" "dessous")
+       ("the" "on" "in" "off" "a" "for" "by" "of" "and" "is" "to" "et" "le" "la" "les" "un" "une")
        t))
  '(reftex-insert-label-flags '("s" "asft"))
  '(reftex-label-alist
