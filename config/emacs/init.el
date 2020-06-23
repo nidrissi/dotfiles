3fa318d381d11b2a7b53961fd913342aa8b48662
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2020-06-23 09:52:37 +0200

    emacs: remove startup-hook

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -9,14 +9,6 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-(add-hook 'emacs-startup-hook
-          (lambda ()
-            (message "Emacs ready in %s with %d garbage collections."
-                     (format "%.2f seconds"
-                             (float-time
-                              (time-subtract after-init-time before-init-time)))
-                     gcs-done)))
-
 (require 'org)                  ; https://stackoverflow.com/a/17422623
 (setq vc-follow-symlinks t)
 ;; use file-truename to avoid symlink being "younger" than the tangled file
